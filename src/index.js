35a0303cb0e8983568d0e4ffdbcfc03e61783a16
Ritchie <info@ritchieanesco.com>
2018-09-23 08:04:06 +0700

    Write Compound Components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Toggle from "../src/toggle";
 
+function logger(log) {
+  console.log("onToggle", log);
+}
+
 function App() {
-  return <Toggle onToggle={state => console.log("onToggle", state)} />;
+  return (
+    <Toggle onToggle={logger}>
+      <Toggle.Button />
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+    </Toggle>
+  );
 }
 
 const rootElement = document.getElementById("root");
--- src/toggle.js
+++ src/toggle.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import { Switch } from "../src/switch";
 
+//Compound components give more rendering control to the user.
+//The functionality of the component stays intact while how it looks and the order of the children can be changed at will.
+//We get this functionality by using the special React.Children.map function to map over the children given to our <Toggle/> component.
+//We map over the children to pass the on state as a prop to its children.
+//We move the visual pieces of the component out into function components and add them as static properties to <Toggle/>.
+
 class Toggle extends Component {
+  static On = ({ on, children }) => (on ? children : null);
+  static Off = ({ on, children }) => (on ? null : children);
+  static Button = ({ on, toggle, ...props }) => (
+    <Switch on={on} onClick={toggle} {...props} />
+  );
+  // these static methods are compound components.
+  // to give access to on, props, and toggle the children
+  // need to be hooked up via the render method
   state = { on: false };
   toggle = () => {
     this.setState(
@@ -13,7 +27,12 @@ class Toggle extends Component {
   };
 
   render() {
-    return <Switch on={this.state.on} onClick={this.toggle} />;
+    return React.Children.map(this.props.children, child =>
+      React.cloneElement(child, {
+        on: this.state.on,
+        toggle: this.toggle
+      })
+    );
   }
 }
 export default Toggle;
