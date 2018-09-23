287d6454c9461de4b1024ab25e42fecfba738a61
Ritchie <info@ritchieanesco.com>
2018-09-23 10:11:26 +0700

    Make Compound React Components Flexible

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ function logger(log) {
 }
 
 function App() {
+  //using reacts context api to pass down state and props to children
+  //without requiring any specific structure or order
   return (
     <Toggle onToggle={logger}>
-      <Toggle.Button />
+      <div>
+        <Toggle.Button />
+      </div>
       <Toggle.On>The button is on</Toggle.On>
       <Toggle.Off>The button is off</Toggle.Off>
     </Toggle>
--- src/toggle.js
+++ src/toggle.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 import { Switch } from "../src/switch";
 
-//Compound components give more rendering control to the user.
-//The functionality of the component stays intact while how it looks and the order of the children can be changed at will.
-//We get this functionality by using the special React.Children.map function to map over the children given to our <Toggle/> component.
-//We map over the children to pass the on state as a prop to its children.
-//We move the visual pieces of the component out into function components and add them as static properties to <Toggle/>.
+//Our current compound component implementation is great, but it's limited in that users cannot render the structure they need.
+//Let's allow the user to have more flexibility by using React context to share the implicit state to our child <Toggle/> components.
+//We will walk through using React's official context API with React.createContext and use the given Provider and Consumer components
+//to share state implicitly between our compound components giving our users the flexibility they need out of our component.
+
+const ToggleContext = React.createContext();
+//This is the key that connects state and props to children
 
 class Toggle extends Component {
-  static On = ({ on, children }) => (on ? children : null);
-  static Off = ({ on, children }) => (on ? null : children);
-  static Button = ({ on, toggle, ...props }) => (
-    <Switch on={on} onClick={toggle} {...props} />
+  static On = ({ children }) => (
+    <ToggleContext.Consumer>
+      {contextValue => (contextValue.on ? children : null)}
+    </ToggleContext.Consumer>
   );
-  // these static methods are compound components.
-  // to give access to on, props, and toggle the children
-  // need to be hooked up via the render method
+  static Off = ({ children }) => (
+    <ToggleContext.Consumer>
+      {contextValue => (contextValue.on ? null : children)}
+    </ToggleContext.Consumer>
+  );
+  static Button = props => (
+    <ToggleContext.Consumer>
+      {contextValue => (
+        <Switch on={contextValue.on} onClick={contextValue.toggle} {...props} />
+      )}
+    </ToggleContext.Consumer>
+  );
+  //using createContext consumer to provider the state and props
   state = { on: false };
   toggle = () => {
     this.setState(
@@ -27,11 +39,16 @@ class Toggle extends Component {
   };
 
   render() {
-    return React.Children.map(this.props.children, child =>
-      React.cloneElement(child, {
-        on: this.state.on,
-        toggle: this.toggle
-      })
+    //use provider to pass down state and toggle to the static methods
+    return (
+      <ToggleContext.Provider
+        value={{
+          on: this.state.on,
+          toggle: this.toggle
+        }}
+      >
+        {this.props.children}
+      </ToggleContext.Provider>
     );
   }
 }
