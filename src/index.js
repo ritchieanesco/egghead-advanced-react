3ae6a351c5c58b079569f7ed94e77a1fd77639cb
Ritchie <info@ritchieanesco.com>
2018-09-24 11:28:04 +0700

    Use Prop Getters with Render Props

--- src/index.js
+++ src/index.js
@@ -7,17 +7,28 @@ function logger(log) {
   console.log("onToggle", log);
 }
 
+function alertTest() {
+  alert("test");
+}
+
 function App() {
   return (
     <Toggle onToggle={logger}>
-      {({ on, togglerProps }) => (
-        //togglerProps contains all the attributes, callbacks which can
-        //be destructured in a element
+      {({ on, getTogglerProps }) => (
+        //getTogglerProps has the ability to pass in additional
+        // attributes and events which can then be destructured
+        //in the component
         <div>
           {on ? "The button is on" : "The button is off"}
-          <Switch on={on} {...togglerProps} />
+          <Switch on={on} {...getTogglerProps()} />
           <hr />
-          <button aria-label="custom-button" {...togglerProps}>
+          <button
+            {...getTogglerProps({
+              "aria-label": "custom-button",
+              id: "custom-button-id",
+              onClick: alertTest
+            })}
+          >
             {on ? "on" : "off"}
           </button>
         </div>
--- src/toggle.js
+++ src/toggle.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import { Switch } from "../src/switch";
 
-//Sometimes you have common use cases that require common props to be applied to certain elements.
-//You can collect these props into an object for users to simply apply to their elements and we'll see how to do that in this lesson.
+//When you're using prop collections, sometimes you can run into trouble with exposing implementation details of your prop collections.
+//Abstract that away by simply creating a function called a prop getter that will compose the user's props with our prop collection.
+
+const runFns = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args));
+//utility to pass in a list of arguments to execute
 
 class Toggle extends Component {
   state = { on: false };
@@ -19,13 +22,18 @@ class Toggle extends Component {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      // below can be passed to element and destructured
-      togglerProps: {
-        onClick: this.toggle,
-        "aria-pressed": this.state.on
-      }
+      // provide a function to retrieve all props
+      getTogglerProps: this.getTogglerProps
     };
   }
+
+  getTogglerProps = ({ onClick, ...props } = {}) => {
+    return {
+      onClick: runFns(onClick, this.toggle),
+      "aria-pressed": this.state.on,
+      ...props
+    };
+  };
 
   render() {
     //remove all rendering to props and provide the rendering all
